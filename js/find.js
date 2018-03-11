74b0a2469a9ac25aa168abe13d5096a990f04a8b
GodStick <499321348@qq.com>
2018-03-11 16:12:50 +0800

    version0.9

--- js/find.js
+++ js/find.js
@@ -1,5 +1,29 @@
 function Findnumber()
 {
 	var x = document.getElementById("user").value;
-	alert(x);
+	var mychar = document.getElementById("classn");
+	var yearnum = x.substr(0,2);
+	var mojar = x.substr(2,4);
+	var classnum = x.substr(6,2);
+	var mojarname;
+	var mojarnum = parseInt(mojar);
+	var classname = yearnum + "" + classnum;
+	switch(mojarnum)
+	{
+		case 1101:
+			mojarname = "算机";
+			break;
+		case 1102:
+			mojarname = "通信";
+			break;
+		case 1103:
+			mojarname = "软件";
+			break;
+		case 1104:
+			mojarname = "数媒";
+			break;
+
+	}
+	mychar.innerHTML = mojarname + classname;
 }
+

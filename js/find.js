3edd485a2ff62f55c1bb4f30f07a8d4c16d50b96
GodStick <499321348@qq.com>
2018-03-12 16:19:24 +0800

    version0.9.7

--- js/find.js
+++ js/find.js
@@ -22,6 +22,51 @@ function Findnumber()
 		case 1104:
 			mojarname = "数媒";
 			break;
+		case 1201:
+			mojarname = "材化";
+			break;
+		case 1205:
+			mojarname = "复材";
+			break;
+		case 1203:
+			mojarname = "高材";
+			break;
+		case 1206:
+			mojarname = "高材卓越";
+			break;
+		case 1202:
+			mojarname = "化学";
+			break;
+		case 0101:
+			mojarname = "化工";
+			break;
+		case 0104:
+			mojarname = "化工卓越";
+			break;
+		case 0901:
+			mojarname = "环工";
+			break;
+		case 0902:
+			mojarname = "环科";
+			break;
+		case 0103:
+			mojarname = "能源";
+			break;
+		case 0903:
+			mojarname = "生工";
+			break;
+		case 1204:
+			mojarname = "非金";
+			break;
+		case 0102:
+			mojarname = "应化";
+			break;
+		case 0206:
+			mojarname = "安工";
+			break;
+		case 0202:
+			mojarname = "材料";
+			break;
 
 	}
 	mychar.innerHTML = mojarname + classname;
